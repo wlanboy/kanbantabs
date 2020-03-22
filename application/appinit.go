90ac390d358919b59e25823beee00b095f3d7ae7
wlanboy <me@wlanboy.com>
2020-03-22 23:04:53 +0100

    Added init function to load Kanban struct from data file

---
+++ application/appinit.go
@@ -0,0 +1,19 @@
+package application
+
+import (
+	store "../store"
+	"github.com/joho/godotenv"
+)
+
+/*Initialize configuration*/
+func (kanban *Kanban) Initialize() {
+	godotenv.Load()
+
+	kanban.Filename = "./.kanbantabs"
+
+	var storage store.Storage = store.Storage{}
+	storage.Filename = kanban.Filename
+	storage.Load()
+
+	kanban.Storage = &storage
+}
