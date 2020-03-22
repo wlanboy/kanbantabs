067a9dfb267221d5f7b68734b64bbf268d2d5284
wlanboy <me@wlanboy.com>
2020-03-22 23:06:21 +0100

    Added os Args parsing and execute delegation

---
+++ application/appcommands.go
@@ -0,0 +1,35 @@
+package application
+
+import (
+	"fmt"
+	"os"
+
+	comms "../commands"
+)
+
+/*RunCommands for Kanban*/
+func (kanban *Kanban) RunCommands(args []string) {
+	if len(args) == 1 {
+		comms.Show(kanban.Storage)
+	} else if len(args) == 2 {
+		showHelp()
+	} else if len(args) == 3 {
+		object := args[1]
+		verb := args[2]
+		comms.ExecuteOn(kanban.Storage, object, verb, "")
+		comms.Show(kanban.Storage)
+	} else if len(args) == 4 {
+		object := args[1]
+		verb := args[2]
+		param := args[3]
+		comms.ExecuteOn(kanban.Storage, object, verb, param)
+		comms.Show(kanban.Storage)
+	} else if len(args) > 4 {
+		showHelp()
+	}
+}
+
+func showHelp() {
+	fmt.Println("Usage:", os.Args[0], "verb", "object")
+	fmt.Println("Objects (verbs): lane (add, delete), card (add, delete, move)")
+}
