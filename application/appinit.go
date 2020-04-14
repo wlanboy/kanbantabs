5144c224e61ba8dfbe577121f8031233b91f1ff2
wlanboy <me@wlanboy.com>
2020-04-14 09:28:53 +0200

    Added homedir because golang does not like tilde

--- application/appinit.go
+++ application/appinit.go
@@ -1,6 +1,9 @@
 package application
 
 import (
+	"os/user"
+	"path/filepath"
+
 	store "../store"
 	"github.com/joho/godotenv"
 )
@@ -9,7 +12,8 @@ import (
 func (kanban *Kanban) Initialize() {
 	godotenv.Load()
 
-	kanban.Filename = "~/.kanbantabs"
+	usr, _ := user.Current()
+	kanban.Filename = filepath.Join(usr.HomeDir, ".kanbantabs")
 
 	var storage store.Storage = store.Storage{}
 	storage.Filename = kanban.Filename
